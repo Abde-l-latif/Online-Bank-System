36a59ba6406c520931fefe2d3e5000a1e32f8f49
unknown <outegnitabdellatif@gmail.com>
2026-07-28 23:45:13 +0100

    >>

--- BankFrontEnd/bank-app/src/Pages/Home/Home.jsx
+++ BankFrontEnd/bank-app/src/Pages/Home/Home.jsx
@@ -3,10 +3,51 @@ import Top from "../../Components/Top/Top"
 import Authentication from "../../Components/Authentication/Authentication"
 import Style from "./Home.module.css"
 import BankCard from "../../Components/BankCard/BankCard"
+import { useEffect, useState} from "react"
 
 
 export default function Home()
 {
+    const [data , setData ] = useState(null);
+    const [error, setError] = useState(null)
+
+    useEffect(() =>
+    {
+        let isMounted = true;
+
+        async function fetchRates() {
+            try {
+
+                const response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/mad.json');
+
+                if (response.ok) {
+                    const responseData = await response.json();
+
+                    if(isMounted)
+                    {
+                        setData(responseData);
+                        console.log(responseData);
+                    }
+                }
+                
+            } catch (err) {
+
+                if(isMounted)
+                {
+                    setError('Fetch failed: ' + err.message);
+                }
+            }
+        } 
+
+        fetchRates();
+
+        return () => {
+            isMounted = false;
+        };
+    }
+    , []
+    )
+
     return (
         <>
             <Top/>
@@ -17,7 +58,12 @@ export default function Home()
                     <p>Vos services bancaires accessibles en un seul clic</p>
                     <button> En savoir plus </button>
                 </aside>
-            
+                <div className={Style.Currency}>
+                    <h2>CONVERTIR EUROS ET USDS EN DIRHAMS MAROCAINS</h2>
+                    <div className={Style.CurrencyData}>
+                        
+                    </div>
+                </div>
             </section>
         </>
     )
